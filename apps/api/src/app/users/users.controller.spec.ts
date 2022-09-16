0e107c8d22c703302ec5f32ee9639e852a7e399b
Abdul Rehman <abdulrehman@Abduls-Air.localdomain>
2022-09-16 15:51:41 +0500

    Some Test Cases Written

--- apps/api/src/app/users/users.controller.spec.ts
+++ apps/api/src/app/users/users.controller.spec.ts
@@ -1,20 +1,61 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Users } from '@prisma/client';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-
 describe('UsersController', () => {
   let controller: UsersController;
-
+  let fakeUsersService: Partial<UsersService>;
   beforeEach(async () => {
+    fakeUsersService = {
+      findOne: (username) => {
+        return Promise.resolve({
+          username,
+          email: 'asdf@asdf.com',
+          name: 'Abdul rehman',
+        } as Users);
+      },
+      findAll: () => {
+        return Promise.resolve([
+          {
+            username: 'abdulrehman',
+            email: 'asdf@asdf.com',
+            name: 'Abdul rehman',
+          } as Users,
+
+          {
+            username: 'affan',
+            email: 'affan@gmail.com',
+            name: 'Affan Mustafa',
+          } as Users,
+        ]);
+      },
+    };
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+      ],
     }).compile();
-
     controller = module.get<UsersController>(UsersController);
   });
-
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+  it('findAllUsers returns all Users', async () => {
+    const users = await controller.findAll();
+    expect(users.length).toBeGreaterThan(0);
+  });
+  it('findOne returns a single user with the given id', async () => {
+    const users = await controller.findOne('abdulrehman');
+    expect(users).toBeDefined();
+  });
+  it('findOne throws an error if the user with the given username is not found', async () => {
+    fakeUsersService.findOne = () => null;
+    try {
+      await controller.findOne('abdulrehman');
+    } catch (error) {}
   });
 });
--- apps/api/src/app/users/users.service.spec.ts
+++ apps/api/src/app/users/users.service.spec.ts
@@ -1,15 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { PrismaService } from '../prisma/prisma.service';
+
+let mockUsersRepository = () => ({
+  findAll: jest.fn(),
+  findOne: jest.fn(),
+});
+
+const mockUser = {
+  username: 'AbdulRehman',
+  name: 'Abdul Rehman Hameedullah',
+  email: 'abdulrehman@gmail.com',
+};
 
 describe('UsersService', () => {
   let service: UsersService;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        { provide: PrismaService, useValue: mockUsersRepository },
+      ],
     }).compile();
 
-    service = module.get<UsersService>(UsersService);
+    service = module.get(UsersService);
+    prisma = module.get(PrismaService);
   });
 
   it('should be defined', () => {
